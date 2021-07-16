b656f7f1bff559d85e0047c1b7e894b44947a982
jayasrivanapalli <87139788+jayasrivanapalli@users.noreply.github.com>
2021-07-16 09:39:45 +0530

    Assignment2 submitted

---
+++ Operations.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class Operations
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+ArrayList<Integer> A=new ArrayList<Integer>();
+int x=sc.nextInt();
+int y=sc.nextInt();
+int z=sc.nextInt();
+A.add(x);
+A.add(y);
+A.add(z);
+
+ArrayList<Integer> B=new ArrayList<Integer>();
+{
+Collections.sort(A);
+}
+ArrayList<Integer> C=new ArrayList<Integer>();
+{
+Collections.reverse(A);
+}
+ArrayList<Integer> S=new ArrayList<Integer>();
+{
+	int g;
+g=A.size();
+}
+ArrayList<Integer> D=new ArrayList<Integer>();
+{
+Collections.sort(A,Collections.reverseOrder());
+}
+{
+for(Integer i:A)
+{
+	System.out.print(i+" ");
+}
+}
+}
+}
